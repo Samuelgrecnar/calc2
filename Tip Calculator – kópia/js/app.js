6667067ac71c7b650b3410bbcc7daf4fa574fa34
Samuel Grecnar <140975233+Samuelgrecnar@users.noreply.github.com>
2023-09-06 18:52:41 +0200

    Calculator
    Calculator by JS and HTML and CSS

---
+++ Tip Calculator – kópia/js/app.js
@@ -0,0 +1,14 @@
+function toDisplay(value){
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = "";
+}
+
+function calculate() {
+    let expr = document.getElementById('display').value;
+    let result = eval(expr)
+
+    document.getElementById('display').value = result
+}
